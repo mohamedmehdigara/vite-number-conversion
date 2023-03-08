d27a0c66e1c84b50f37e37230401cb07f164e316
mohamedmehdigara <garamehdi57@yahoo.com>
2023-03-08 10:20:46 +0100

    added files

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import  React from "react"
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {  BrowserRouter } from 'react-router-dom';
+
+
+const RootComponent = () => (
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+          </BrowserRouter>
+      </React.StrictMode>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RootComponent />);
